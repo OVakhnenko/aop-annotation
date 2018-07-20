d1f9a37e3cc6c356f133e4ab874ce443db565b69
ovakhnenko <gmail@vakhnenko.com>
2018-07-20 09:11:52 +0300

    ...

---
+++ src/main/java/net/proselyte/aop/AopDeveloperRunner.java
@@ -0,0 +1,15 @@
+package net.proselyte.aop;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AopDeveloperRunner {
+    public static void main(String[] args) {
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("aop-developer-annotations-config.xml");
+        Developer developer = (Developer) context.getBean("developer");
+
+        System.out.println(developer);
+        developer.throwSomeMysticException();
+    }
+}
